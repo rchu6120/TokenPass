da243b48c99ec43624f5c22ab7adbe62047ec798
Ricky <rchu9700@bths.edu>
2018-12-20 23:44:09 -0500

    done

--- src/TokenPass.java
+++ src/TokenPass.java
@@ -1,12 +1,14 @@
 public class TokenPass {
     private int[] board;
     private int currentPlayer;
+    private int playerCount;
     public TokenPass(int playerCount) {
         this.board = new int[playerCount];
-        for (int i = 0; i < board.length; i++) {
+        for (int i = 0; i < playerCount; i++) {
             this.board[i] = (int)(Math.random() * 10) + 1;
         }
         this.currentPlayer = (int)(Math.random() * playerCount);
+        this.playerCount = playerCount;
     }
     public void distributeCurrentPlayerTokens() {
         int nextPlayer = currentPlayer;
@@ -20,6 +22,13 @@ public class TokenPass {
     }
     public void nextPlayer() {
         currentPlayer++;
+        currentPlayer = currentPlayer % playerCount;
+    }
+    public void printBoard() {
+        for (int i = 0; i < playerCount; i++) {
+            System.out.print("Player " + i + ": " + board[i] + " " );
+        }
+        System.out.println("\n" + "Current Player: " + currentPlayer);
     }
     public int gameOver() {
         int result = -1;
@@ -29,11 +38,5 @@ public class TokenPass {
             }
         }
         return result;
-    }
-    public String printBoard() {
-       String str = "";
-        for (int i = 0; i < board.length; i++) {
-
-        )
     }
 }
